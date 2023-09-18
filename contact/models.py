8948560cdba8e01c0c82b99c16b71f98fb6a087a
¨Juliano <julianometzker87@gmail.com>
2023-09-17 22:41:54 -0300

    first model | makemigrations and migrate

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+# first_name (string), last_name (string) , phone (string)
+# email (email), created_date (date) description (text)
+# category (foreign key), show (boolean), owner (foreign key)
+# picture (imagem)
 
-# Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField('Name', max_length=100, blank=False )
+    last_name  = models.CharField("Surname",max_length=254,blank=True,)
+    phone = models.CharField('Phone', max_length=11)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
